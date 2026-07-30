99e0335b1bf070ec633b6c1dab134f94ea7625ec
soumen2375 <soumenmaitymail2375@gmail.com>
2026-07-30 11:39:29 +0530

    fix: update Vite base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/vfx_web/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
